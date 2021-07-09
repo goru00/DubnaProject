408afde7734463e96f09dd7a3fc568d86ca8d2d9
goru00 <d-tretyakov-artim2012@yandex.ru>
2021-07-09 13:20:06 +0000

    Created client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'admin',
+    password: '1234',
+    database: 'mfc'
+});
+
+
+app.get("/api/auth", (req, res) => {
+    const insertSQL = "INSERT INTO Users VALUES (?, ?);"
+    db.query(insertSQL, [login, pass], (err, result) => {});
+});
 
 app.listen(3001, () => {
     console.log('RUN');
-})
+});
