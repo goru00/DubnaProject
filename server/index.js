593ba07307882e15ee8ccb4f7fd102964854750a
goru00 <d-tretyakov-artim2012@yandex.ru>
2021-07-10 18:02:00 +0000

    Completed server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const db = require('./models');
+const { Users } = require('./models');
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'admin',
-    password: '1234',
-    database: 'mfc'
+const bcrypt = require('bcrypt');
+const cookie = require('cookie-parser');
+
+const { createToken, validateToken } = require('./JWT');
+
+app.use(express.json());
+app.use(cookie());
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await Users.findOne({ where: {username} });
+    if (!user) res.status(400).json({ error: "Такого пользователя не существует!"});
+
+    const dbPass = user.password;
+    bcrypt.compare(password, dbPass).then((match) => {
+        if (!match) {
+            res.status(400).json({error: "Неправильно введен логин или пароль пользователя!"});
+        } else {
+            const accessToken = createToken(user);
+            res.cookie("access-token", accessToken, {
+                maxAge: 60 * 60 * 24 * 30 * 1000,
+                httpOnly: true,
+            });
+            res.json("LOGIN!");
+        }
+    });
 });
 
-
-app.get("/api/auth", (req, res) => {
-    const insertSQL = "INSERT INTO Users VALUES (?, ?);"
-    db.query(insertSQL, [login, pass], (err, result) => {});
+app.get("/profile", validateToken, (req, res) => {
+    res.json("profile");
 });
-
-app.listen(3001, () => {
-    console.log('RUN');
+  
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+      console.log("SERVER RUNNING ON PORT 3001");
+    });
 });
