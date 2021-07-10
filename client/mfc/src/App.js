2c46cd7ad6f47c0026d2f78b9798963e15de8026
goru00 <d-tretyakov-artim2012@yandex.ru>
2021-07-10 18:18:48 +0000

    Created client

--- client/mfc/src/App.js
+++ client/mfc/src/App.js
@@ -1,14 +1,42 @@
 import { useState } from 'react';
 import './App.css';
-import AuthComponent from './auth/AuthComponent';
+import Axios from 'axios';
 
 function App() {
   const [login, setLogin] = useState("");
   const [pass, setPass] = useState("");
+  const [LoginStatus, setLoginStatus] = useState("");
+
+  Axios.defaults.withCredentials = true;
+
+  const auth = () => {
+    Axios.post('http://localhost:3001/login', {
+      username: login, 
+      password: pass,
+    }).then((response) => {
+      if (!response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].login);
+      }
+    });
+  }
 
   return (
     <div className="App">
-      <AuthComponent login={setLogin} pass={setPass} />
+      <div className="authorizationPage">
+            <img width="256" height="256"/>
+              <p>
+                  Логин / имя пользователя
+              </p>
+              <input type="text" className="loginForm" onChange={(e) => setLogin(e.target.value)}/>
+              <p>
+                  Пароль
+              </p>
+              <input type="password" className="passForm" onChange={(e) => setPass(e.target.value)}/>
+              <br />
+              <button onClick={auth}>Авторизоваться</button>
+        </div>
     </div>
   );
 }
